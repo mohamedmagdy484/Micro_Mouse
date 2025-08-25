36672c381de70083a685c6e5bd5bf184e0c148d1
Mohamed Magdy <62652769+mohamedmagdy484@users.noreply.github.com>
2025-08-25 23:51:22 +0300

    Update DC_Motor.c

--- motor_micromouse/HAL/DC_Motor.c
+++ motor_micromouse/HAL/DC_Motor.c
@@ -15,36 +15,33 @@
 
 void DC_RIGHT_INIT(void)
 {
-    // Set IN1 and IN2 (motor right direction pins) as output
     DC_MOTOR_RIGHT_DDR |= (1 << DC_MOTOR_RIGHT_IN1) | (1 << DC_MOTOR_RIGHT_IN2);
 
-    // Set PWM pin (OC0 -> PB3) as output
     PWM_RIGHT_DDR |= (1 << PWM_RIGHT_PIN);
 
-    // Set Timer0 to Fast PWM mode, non-inverting
-    TCCR0 = (1 << WGM00) | (1 << WGM01)   // Fast PWM Mode
-           | (1 << COM01)|            // Non-Inverting Mode
-		   (1<<CS01) | (1<<CS00);;   // Prescaler = 1024
+    TCCR0 = (1 << WGM00) | (1 << WGM01)   
+           | (1 << COM01)|           
+		   (1<<CS01) | (1<<CS00);;   
 
-    // Stop motor initially
+
     DC_MOTOR_RIGHT_PORT &= ~((1 << DC_MOTOR_RIGHT_IN1) | (1 << DC_MOTOR_RIGHT_IN2));
-    OCR0 = 0; // 0% duty cycle
+    OCR0 = 0; 
 }
 void DC_LEFT_INIT(void){
-    // Set IN3 and IN4 as output (خليهم مثلاً PC2 و PC3)
+  
     DC_MOTOR_LEFT_DDR |= (1 << DC_MOTOR_LEFT_IN1) | (1 << DC_MOTOR_LEFT_IN2);
 
-    // Set PWM pin (OC2 -> PD7) as output
+  
     PWM_LEFT_DDR |= (1 << PWM_LEFT_PIN);
 
-    // Set Timer2 to Fast PWM mode, non-inverting
-    TCCR2 = (1 << WGM20) | (1 << WGM21)   // Fast PWM Mode
-           | (1 << COM21)|                 // Non-Inverting Mode
-		   (1<<CS22);   // Prescaler = 1024
+ 
+    TCCR2 = (1 << WGM20) | (1 << WGM21)  
+           | (1 << COM21)|               
+		   (1<<CS22);   
 
-    // Stop motor initially
+  
     DC_MOTOR_LEFT_PORT &= ~((1 << DC_MOTOR_LEFT_IN1) | (1 << DC_MOTOR_LEFT_IN2));
-    OCR2 = 0; // 0% duty cycle
+    OCR2 = 0; 
 
 
 }
@@ -53,7 +50,7 @@ void DC_RIGHT(DcMotor_State state, uint8 speed)
 {
     if (speed > 100) speed = 100;
 
-    // Convert % to 8-bit PWM value (0-255)
+ 
     OCR0 = (uint8)((speed * 255) / 100);
 
     switch (state)
@@ -78,7 +75,7 @@ void DC_LEFT(DcMotor_State state, uint8 speed)
 {
     if (speed > 100) speed = 100;
 
-    // Convert % to 8-bit PWM value (0-255)
+  
     OCR2 = (uint8)((speed * 255) / 100);
 
     switch (state)
@@ -97,3 +94,4 @@ void DC_LEFT(DcMotor_State state, uint8 speed)
             break;
     }
 }
+
